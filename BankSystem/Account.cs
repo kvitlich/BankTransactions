0ea9719e9f2b35e05c54fd0eee0e414369db8ff3
DinaraZZ <dinara.rakhymbekova@gmail.com>
2019-12-09 21:32:04 +0600

    Add project files.

---
+++ BankSystem/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankSystem
+{
+    public class Account
+    {
+        public string Name { get; private set; }
+        public int Summ { get; private set; } = 1000;
+       
+        public Account(string name)
+        {
+            Name = name;
+        }
+
+        public void ProcessSumm(int summ)
+        {
+            Summ += summ;
+            Console.WriteLine($"{this.Name}: {Summ}");
+        }
+
+        public void Send(object arg)
+        {
+            var args = arg as object[];
+            Account dest = (Account)args[0];
+            int summ = (int)args[1];
+            if (summ <= 0)
+                return;
+            this.ProcessSumm(-summ);
+            dest.ProcessSumm(summ);
+        }
+    }
+}
---
+++ BankSystem/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace BankSystem
+{
+    class Program
+    {
+        private static object SyncObject = new object();
+        public static List<Account> Accounts = new List<Account>();
+
+        static void Main(string[] args)
+        {
+            Accounts.Add(new Account("Kvit"));
+            Accounts.Add(new Account("Sarsen"));
+            Accounts.Add(new Account("Bekbolat"));
+            Accounts.Add(new Account("Sax"));
+            Accounts.Add(new Account("Adolf"));
+
+
+            for (int j = 0; j < Accounts.Count; j++)
+            {
+                object[] arguments;
+                lock (SyncObject)
+                {
+                    if (j == Accounts.Count - 1)
+                        arguments = new object[] { Accounts[0], 100 };
+                    else
+                        arguments = new object[] { Accounts[j + 1], 100 };
+                    ThreadPool.QueueUserWorkItem(Accounts[j].Send, arguments);
+                }
+            }
+            //   Thread.Sleep(1000);
+            Console.ReadKey();
+            Console.WriteLine("******************");
+            for (int j = 0; j < Accounts.Count; j++)
+            {
+                Console.WriteLine($"{Accounts[j].Name}: {Accounts[j].Summ}");
+            }
+
+        }
+    }
+}
